85e3326c309ee8d3b7f49dbc9656737e9480bf88
Meriem30 <m.arbaoui@esi-sba.dz>
2023-07-13 17:55:25 +0200

    run.sh modified

--- run.sh
+++ run.sh
@@ -5,29 +5,28 @@
 
 
 ####### FedAvg
-python main_n_clients.py --device 'cpu' --alg 'fedavg' --iters 200 --epochs 3 --batch 32 --lr 0.001
- --dataset 'cifar10' --balance False  --unbalance_sgm 0.3 --partition_data 'dirichlet' --diralpha 0.3 --download True --preprocess True --verbose True
+python main_n_clients.py --device 'cuda' --alg 'fedavg' --iters 250 --epochs 5 --batch 16 --lr 0.001
+--dataset 'cifar10' --balance True --partition_data 'iid' --download True --preprocess True --verbose True
 
 
 ####### FedDyn
-python main_n_clients.py --device 'cpu' --alg 'feddyn' --iters 200 --epochs 3 --batch 32 --lr 0.001
- --dataset 'cifar10' --balance False  --unbalance_sgm 0.3 --partition_data 'dirichlet' --diralpha 0.3
+python main_n_clients.py --device 'cuda' --alg 'feddyn' ---iters 250 --epochs 5 --batch 16 --lr 0.001
+--dataset 'cifar10' --balance True --partition_data 'iid' --download True --preprocess True --verbose True
+
 
 ####### Power-of-choice
-python main_n_clients.py --device 'cpu' --alg 'powerofchoice' --iters 200 --epochs 3 --batch 32 --lr 0.001
- --dataset 'cifar10' --balance False  --unbalance_sgm 0.3 --partition_data 'dirichlet' --diralpha 0.3
+python main_n_clients.py --device 'cuda' --alg 'powerofchoice' --iters 250 --epochs 5 --batch 16 --lr 0.001 -
+-dataset 'cifar10' --balance True --partition_data 'iid' --download True --preprocess True --verbose True
 
 ####### FedBN
-python main_n_clients.py --device 'cpu' --alg 'fedbn' --iters 200 --epochs 3 --batch 32 --lr 0.001
-  --dataset 'cifar10' --balance False  --unbalance_sgm 0.3 --partition_data 'dirichlet' --diralpha 0.3
+python main_n_clients.py --device 'cuda' --alg 'fedbn' --iters 250 --epochs 5 --batch 16 --lr 0.001
+--dataset 'cifar10' --balance True --partition_data 'iid' --download True --preprocess True --verbose True
+
 ####### FedPer
 #python main_n_clients.py --device 'cpu' --alg 'fedper' --iters 200 --epochs 3 --batch 32 --lr 0.001
 #--dataset 'cifar10' --balance False  --unbalance_sgm 0.3 --partition_data 'dirichlet' --diralpha 0.3
 
 #***************************** Plot the results (accuracy and loss) of all the algo in one figure
 python n_client_plot_acc.py
-
-# python main_n_clients.py --device 'cpu' --alg 'fedavg' --iters 200 --epochs 3 --batch 32 --lr 0.001 --dataset 'cifar10' --balance True --partition_data 'iid' --download True --preprocess True --verbose True
-
 
 
